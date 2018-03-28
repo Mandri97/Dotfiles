95b92dd1574d5b17675e4585abf70f14c03ad9a6
Mandri97 <manoa.andriamirado@gmail.com>
2018-03-28 10:56:30 +0300

    modified:   Xresources 	new file:   script/tmux_env.sh

---
+++ script/tmux_env.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Customization tmux Environments
+	 
+SESS=Main
+
+Main(){
+    tmux new-session -s $SESS -n Terminal -d
+    tmux new-window -n Music
+    tmux send-keys -t $SESS:2 'ncmpcpp play; ncmpcpp' C-m 
+    tmux select-window -t $SESS:1
+}
+
+python(){
+    tmux new-session -s $SESS -n Code -d
+    tmux send-keys -t $SESS 'cd $HOME/Code/python/Data_Analysis' C-m
+    tmux split-window -h -t $SESS
+    tmux send-keys -t $SESS 'cd $HOME/Code/python/Data_Analysis' C-m
+    tmux send-keys -t $SESS:1.1 'vim .' C-m
+    tmux send-keys -t $SESS "ipython" C-m
+}
+
+cpp(){
+    tmux new-session -s $SESS -n Code -d
+    tmux send-keys -t $SESS 'cd $HOME/Code/C++/Practical' C-m
+    tmux split-window -h -t $SESS
+    tmux send-keys -t $SESS 'cd $HOME/Code/C++/Practical' C-m
+    tmux send-keys -t $SESS:1.1 'vim .' C-m
+}
+
+case "$1" in 
+    Main|main)
+	$(Main);;
+    Python|python)
+	SESS=Python
+	$(python);;
+    c|cpp|c)
+	SESS=cpp
+	$(cpp);;
+    *)
+	$(Main);;
+esac
+
+tmux select-pane -t $SESS:1.1
+tmux attach -t $SESS
